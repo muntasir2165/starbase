e114c6ea197e9b2d8a5119f7fbf4611342fde5ac
Muntasir Biojid <muntasir2165@hotmail.com>
2020-06-02 11:12:39 -0700

    v-27 introducing the api

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <h3>{{title}}</h3>
+    <button class="btn btn-primary" @click="fetchCharacter">Test Fetch Character</button>
   </div>
 </template>
 
@@ -9,6 +10,13 @@ export default {
   name: "app",
   data() {
     return { title: "Generate Your Team" };
+  },
+  methods: {
+    fetchCharacter() {
+      fetch("https://swapi.dev/api/people/1", { method: "GET" })
+        .then(response => response.json())
+        .then(json => console.log(json));
+    }
   }
 };
 </script>
